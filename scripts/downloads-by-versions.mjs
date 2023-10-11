0b217bbba3c17b3be9f296123e67a2c1a24805aa
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-11 21:33:55 +0700

    move to scoped package: `core-js-bundle` -> `@core-js/bundle`

--- scripts/downloads-by-versions.mjs
+++ scripts/downloads-by-versions.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-top-level-await -- false positive */
 import semver from 'semver';
 
 const { coerce, cmp } = semver;
@@ -17,10 +18,9 @@ async function getStat(pkg) {
 
 const [core, pure, bundle] = await Promise.all([
   getStat('core-js'),
-  // eslint-disable-next-line unicorn/prefer-top-level-await -- false positive
   ALL && getStat('core-js-pure'),
-  // eslint-disable-next-line unicorn/prefer-top-level-await -- false positive
   ALL && getStat('core-js-bundle'),
+  ALL && getStat('@core-js/bundle'),
 ]);
 
 for (let [patch, downloads] of Object.entries(core)) {
